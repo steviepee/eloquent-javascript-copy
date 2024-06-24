6bca8a912aad591d88dc24d32bfa2225a60e83e2
steviepee <jimisax@gmail.com>
2024-06-24 14:19:34 -0500

    reeentered ch 2.. issues on chessboard

--- chapter-02/exercises.js
+++ chapter-02/exercises.js
@@ -26,7 +26,12 @@ LOGS =>
 
 */
 
-function triangles() {
+function triangles(n) {
+  let hash = '';
+for (let i = 1; i <= n; i++) {
+  hash += '#';
+  console.log(hash);
+  }
   
 }
 
@@ -48,7 +53,17 @@ on the number:
 */
 
 function fizzBuzz(start, end) {
-  
+  for (i = start; i <= end; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz');
+      } else if (i % 3 === 0) {
+      console.log('fizz');
+      } else if (i % 5 === 0) {
+      console.log('buzz'); 
+      } else {
+      console.log(i);
+      }
+        }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -67,10 +82,10 @@ string that looks like this " # \n# #\n # \n# #"
 example: drawChessboard(4);
 LOGS =>
 
- # #
-# #
- # #
-# #
+ # # #
+# # #
+ # # #
+# # #
 
 exampmle drawChessboard(3);
 LOGS =>
@@ -80,11 +95,40 @@ LOGS =>
  #
 
 */
-
+//if x is three, it should be space hashtag space, hashtag space hashtag, space hashtag space
 function drawChessboard(x) {
+  //create holding string for odd rows(space hash)
+  let chess = ''
+  //create holding string for even rows(hash space hash)
+  let check = ''
+  //create counting variable to start at 0
+  let y = 0;
+  //as long as y is less than half of x
+  while (y <= Math.trunc(x / 2)) {
+    //add one to y
+    y++;
+    //and add one to odd holding string
+    chess += ` #`;
+    //and add one to even holding string(?)
+    check += `# # `;
+    }//create mechanism for counting up to x
+  for (let i = 0; i <= x; i++) {
+    //do nothing if i is 0
+    if (i !== 0) {
+      //if it's the first row
+      //if (i === 1) {
+        //put in the odd string's current value and add a line break
+      console.log(` ${chess}\n`);
+      //on each other odd row
+      } else if (i % 2) {
+        //put in the odd string's current value and add a line break after
+      console.log(` ${chess}\n`);
+      } else {//on the evens,use the even string's value with a line break
+      console.log(`${chess}\n`);
+      }
+    }
+  }
 
-
-}
 
 ////////////////////////////////////////////////////////////////////////////////
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
